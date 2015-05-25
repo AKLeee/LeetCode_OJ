345c2370d7b9a4557bc014fbefe9bea0bbb73b4d
Dehao Li <dehaoli@Dehaos-MacBook-Pro.local>
2015-05-25 16:37:01 -0700

    Solution: using idea of BFS #104

--- Max_Depth_BT#2/MaxDepthOfBT.py
+++ Max_Depth_BT#2/MaxDepthOfBT.py
@@ -9,6 +9,23 @@
 #         self.right = None
 
 class Solution:
-    # @param {TreeNode} root
-    # @return {integer}
-    def maxDepth(self, root):
+	# @param {TreeNode} root
+	# @return {integer}
+	def maxDepth(self, root):
+		maxDep = 0
+		if not root:
+			return 0
+		queue = []
+		queue.append(root)
+		
+		while len(queue)>0:
+			maxDep = maxDep + 1
+			#using extra memory to store nodes on each levels
+			level = []
+			for node in queue:
+				if node.left is not None:
+					level.append(node.left)
+				if node.right is not None:
+					level.append(node.right)
+			queue = level
+		return maxDep
---
+++ Max_Depth_BT#2/max-depth-of-bt-run.py
@@ -0,0 +1,16 @@
+from MaxDepthOfBT import Solution
+from TreeNode import TreeNode
+
+root = TreeNode(1)
+node02 = TreeNode(2)
+node03 = TreeNode(3)
+node04 = TreeNode(4)
+
+root.left = node02
+root.right = node03
+
+node02.left = node04
+
+test = Solution()
+
+print test.maxDepth(root) 
