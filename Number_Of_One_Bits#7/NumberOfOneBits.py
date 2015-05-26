f9e7e71498a2bb694d7a27abd21f52b2668e63eb
AngkouLee <angkoulee@gmail.com>
2015-05-26 16:58:26 -0700

    Solution: using Bit Manipulation, moving right one bit and &, but not improve the performance #191

--- Number_Of_One_Bits#7/NumberOfOneBits.py
+++ Number_Of_One_Bits#7/NumberOfOneBits.py
@@ -1,10 +1,21 @@
+# class Solution:
+#     # @param n, an integer
+#     # @return an integer
+#     def hammingWeight(self, n):
+#     	s = bin(n)[2:]
+#     	count = 0
+#     	for c in s:
+#     		if c == '1':
+#     			count += 1
+#     	return count
+
 class Solution:
     # @param n, an integer
     # @return an integer
     def hammingWeight(self, n):
-    	s = bin(n)[2:]
-    	count = 0
-    	for c in s:
-    		if c == '1':
-    			count += 1
-    	return count
+    	result = 0
+    	#False == 0
+    	while n:
+    		result += n&1
+    		n >>= 1
+    	return result
