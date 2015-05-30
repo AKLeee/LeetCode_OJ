2d44f359a1e4793d0b86253f11f2aaa0772ba184
AngkouLee <angkoulee@gmail.com>
2015-05-30 13:28:01 -0700

    Solution: using Moore Voting Algorithm, no extra space use #169

--- Majority_Num#12/MajorityNum.py
+++ Majority_Num#12/MajorityNum.py
@@ -2,18 +2,37 @@
 
 # You may assume that the array is non-empty and the majority element always exist in the array.
 
+# ---Using hashtable Time O(n) Space O(n)---
+# class Solution:
+#     # @param {integer[]} nums
+#     # @return {integer}
+#     def majorityElement(self, nums):
+#     	if(len(nums) == 1):
+#     		return nums[0]
+#     	hashTable = {}
+#     	for num in nums:
+#     		if hashTable.get(num):
+#     			count = hashTable.get(num)
+#     			hashTable.update({num:count+1})
+#     			if hashTable.get(num) > len(nums)/2:
+#     				return num
+#     		else:   			
+#     			hashTable.update({num:1})
+
+# ---Using Moore Voting algorithm O(n)---
 class Solution:
     # @param {integer[]} nums
     # @return {integer}
     def majorityElement(self, nums):
-    	if(len(nums) == 1):
-    		return nums[0]
-    	hashTable = {}
+    	candidate = None
+    	count = 0
     	for num in nums:
-    		if hashTable.get(num):
-    			count = hashTable.get(num)
-    			hashTable.update({num:count+1})
-    			if hashTable.get(num) > len(nums)/2:
-    				return num
-    		else:   			
-    			hashTable.update({num:1})
+    		if count == 0:
+    			candidate = num
+    			count = 1
+    		else:
+    			if num == candidate:
+    				count += 1
+    			else:
+    				count -= 1
+    	return candidate
--- Majority_Num#12/majority-num-run.py
+++ Majority_Num#12/majority-num-run.py
@@ -1,6 +1,6 @@
 from MajorityNum import Solution
 
-data = [2]
+data = [2,3,4,5,2,4,6,5,4]
 #data = [2,2,2,2,3,4]
 
 test = Solution()
