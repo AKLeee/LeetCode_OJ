6b21464010d03a76024b1883605f44492becfdee
AngkouLee <angkoulee@gmail.com>
2015-05-30 12:47:00 -0700

    Solusion: using hashtable #169

--- Majority_Num#12/MajorityNum.py
+++ Majority_Num#12/MajorityNum.py
@@ -1,4 +1,4 @@
-# Given an array of size n, find the majority element. The majority element is the element that appears more than ⌊ n/2 ⌋ times.
+# Given an array of size n, find the majority element. The majority element is the element that appears more than [n/2] times.
 
 # You may assume that the array is non-empty and the majority element always exist in the array.
 
@@ -6,3 +6,14 @@ class Solution:
     # @param {integer[]} nums
     # @return {integer}
     def majorityElement(self, nums):
+    	if(len(nums) == 1):
+    		return nums[0]
+    	hashTable = {}
+    	for num in nums:
+    		if hashTable.get(num):
+    			count = hashTable.get(num)
+    			hashTable.update({num:count+1})
+    			if hashTable.get(num) > len(nums)/2:
+    				return num
+    		else:   			
+    			hashTable.update({num:1})
---
+++ Majority_Num#12/majority-num-run.py
@@ -0,0 +1,8 @@
+from MajorityNum import Solution
+
+data = [2]
+#data = [2,2,2,2,3,4]
+
+test = Solution()
+
+print test.majorityElement(data)
