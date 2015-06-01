636b0efd818a0192031a69de86b1f08831eef4ef
AngkouLee <angkoulee@gmail.com>
2015-06-01 16:24:37 -0700

    Open question #83

---
+++ Remove_Dup_from_SortedList#14/RemoveDupSL.py
@@ -0,0 +1,12 @@
+# Given a sorted linked list, delete all duplicates such that each element appear only once.
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    # @param {ListNode} head
+    # @return {ListNode}
+    def deleteDuplicates(self, head):
