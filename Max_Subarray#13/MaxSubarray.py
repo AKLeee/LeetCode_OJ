0a9c5c325277cb56bc60ab7b3c9a7c020f7ddc48
AngkouLee <angkoulee@gmail.com>
2015-06-01 16:18:12 -0700

    Solution: one dimension DP #53

--- Max_Subarray#13/MaxSubarray.py
+++ Max_Subarray#13/MaxSubarray.py
@@ -4,3 +4,15 @@ class Solution:
     # @param {integer[]} nums
     # @return {integer}
     def maxSubArray(self, nums):
+    	#only ask for max number, don't care details of subarray
+    	#so each step we compare 3 things: 
+    	#	current num; 
+    	#	global max; 
+    	#	'what current num affects this contiguous' max.
+    	maxResult = nums[0]
+    	maxNow = 0
+
+    	for idx,num in enumerate(nums):
+    		maxNow = max(nums[idx],maxNow+nums[idx])
+    		maxResult = max(maxResult, maxNow)
+    	return maxResult
---
+++ Max_Subarray#13/max-subarray-run.py
@@ -0,0 +1,7 @@
+from MaxSubarray import Solution
+
+data = [-2, 3, 5, -9, 5, 2, 11, -2, -10]
+
+test = Solution()
+
+print test.maxSubArray(data)
