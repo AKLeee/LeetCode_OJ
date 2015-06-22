39f91309545c9dc3074992cd7946063f0a8e6fc8
AngkouLee <angkoulee@gmail.com>
2015-06-22 16:17:13 -0700

    Open question #46

---
+++ Permutations#32/Permutations.py
@@ -0,0 +1,9 @@
+# Given a collection of numbers, return all possible permutations.
+
+class Solution:
+    # @param {integer[]} nums
+    # @return {integer[][]}
+    
+    result = []
+    oneCon = []
+    def permute(self, nums):
---
+++ Permutations#32/permutation-run.py
@@ -0,0 +1,10 @@
+from Permutations import Solution
+
+data = [1,2,3]
+
+test = Solution()
+
+result = test.permute(data)
+
+for r in result:
+	print r
