6fe0c8c0f1dadacd6d5ff052864e0a2ea04c80e7
AngkouLee <angkoulee@gmail.com>
2015-09-07 23:22:32 -0700

    Solution: TRY two pointers #61

---
+++ Rotate_List#101/RotateList.py
@@ -0,0 +1,15 @@
+# Given a list, rotate the list to the right by k places, where k is non-negative.
+
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def rotateRight(self, head, k):
+        """
+        :type head: ListNode
+        :type k: int
+        :rtype: ListNode
+        """
