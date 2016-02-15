b500c4e1416245d1d875cd16406744568cd33ea9
AngkouLee <angkoulee@gmail.com>
2016-02-15 13:48:55 -0700

    Solution: corner case that root may be infinite large or small #98

---
+++ 123#Validate_BST/ValidateBST.py
@@ -0,0 +1,14 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def isValidBST(self, root, lessThan = float('inf'), largerThan = float('-inf')):
+        if not root:
+            return True
+        if root.val <= largerThan or root.val >= lessThan:
+            return False
+        return self.isValidBST(root.left, root.val, largerThan) and self.isValidBST(root.right, lessThan, root.val)
